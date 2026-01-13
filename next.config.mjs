d079c8ee727e85aec6fb109cca2597ec6366b532
armd482 <armd482@naver.com>
2026-01-13 11:49:02 +0900

    fix: turbopack svg 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
   webpack: (config) => {
     config.module.rules.push({
-      test: /\.svg$/,
+      test: /\.svg$/i,
       use: ['@svgr/webpack'],
     });
-
     return config;
   },
 };
