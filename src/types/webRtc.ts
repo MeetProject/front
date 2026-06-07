0a3a85f99b4b7a81121fcda074894b16f538d753
armd482 <armd482@naver.com>
2026-06-07 14:44:26 +0900

    chore: 대소문자 수정

--- src/types/webRtc.ts
+++
@@ -1,9 +0,0 @@
-import { AppData as PrimitiveAppData } from 'mediasoup-client/types';
-
-import { TrackType } from './deviceType';
-
-export type Direction = 'send' | 'recv';
-export type AppData = PrimitiveAppData & {
-  trackType: TrackType;
-  userId: string;
-};
