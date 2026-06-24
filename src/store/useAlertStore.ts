d1d9038037e13dabd52849ad033580dc8b30c505
armd482 <armd482@naver.com>
2026-06-24 16:36:41 +0900

    refactor: alert id를 uuid로 생성

--- src/store/useAlertStore.ts
+++ src/store/useAlertStore.ts
@@ -5,12 +5,12 @@ import { Alert } from '@/types/components';
 interface AlertState {
   alerts: Alert[];
   addAlert: (message: string, duration?: number, onClose?: () => void) => void;
-  removeAlert: (id: number) => void;
+  removeAlert: (id: string) => void;
 }
 
 export const useAlertStore = create<AlertState>((set) => ({
   addAlert: (message, duration = 4000, onClose) => {
-    const id = Date.now();
+    const id = crypto.randomUUID();
     set((state) => ({ alerts: [...state.alerts, { id, message }] }));
 
     setTimeout(() => {
