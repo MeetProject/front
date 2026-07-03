3f25f51d3617492c3a4031dba43d8cc1d09aa1cb
armd482 <armd482@naver.com>
2026-07-03 21:05:13 +0900

    fix: 참가자 검색어 정규식 특수문자 이스케이프 처리

--- src/util/text.ts
+++ src/util/text.ts
@@ -71,6 +71,8 @@ const MIDDLE_OFFSET_RANGE = 28;
 const charCode = (first: number, middle: number, last: number) =>
   String.fromCharCode(OFFSET + first * FIRST_OFFSET_RANGE + middle * MIDDLE_OFFSET_RANGE + last);
 
+const escapeRegExp = (value: string) => value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
 export const charMatcher = (search = '') => {
   if (!search) {
     return /(?:)/;
@@ -79,7 +81,7 @@ export const charMatcher = (search = '') => {
   const regex = FIRST_CONSTANT.reduce(
     (accumulator, first, index) =>
       accumulator.replace(new RegExp(first, 'g'), `[${charCode(index, 0, 0)}-${charCode(index + 1, 0, -1)}]`),
-    search,
+    escapeRegExp(search),
   );
 
   return new RegExp(`(${regex})`);
